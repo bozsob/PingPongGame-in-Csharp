303f74069687467c59e258dc420d767e63edfba8
Beatrix Kerekes Bozsó <bozso.beatrix@gmail.com>
2017-06-09 11:16:03 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PingPongGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameBackGround = new System.Windows.Forms.Panel();
+            this.gameOver = new System.Windows.Forms.Label();
+            this.scorePoints = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.gameBackGround.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameBackGround
+            // 
+            this.gameBackGround.BackColor = System.Drawing.Color.Transparent;
+            this.gameBackGround.Controls.Add(this.gameOver);
+            this.gameBackGround.Controls.Add(this.scorePoints);
+            this.gameBackGround.Controls.Add(this.scoreLabel);
+            this.gameBackGround.Controls.Add(this.ball);
+            this.gameBackGround.Controls.Add(this.player);
+            this.gameBackGround.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameBackGround.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gameBackGround.Location = new System.Drawing.Point(0, 0);
+            this.gameBackGround.Name = "gameBackGround";
+            this.gameBackGround.Size = new System.Drawing.Size(681, 365);
+            this.gameBackGround.TabIndex = 0;
+            
+            // 
+            // gameOver
+            // 
+            this.gameOver.AutoSize = true;
+            this.gameOver.Location = new System.Drawing.Point(354, 13);
+            this.gameOver.Name = "gameOver";
+            this.gameOver.Size = new System.Drawing.Size(197, 148);
+            this.gameOver.TabIndex = 4;
+            this.gameOver.Text = "Game Over\r\nF1 - Easy\r\nF2 - Hard\r\nEsc - Exit";
+            this.gameOver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scorePoints
+            // 
+            this.scorePoints.AutoSize = true;
+            this.scorePoints.Location = new System.Drawing.Point(129, 13);
+            this.scorePoints.Name = "scorePoints";
+            this.scorePoints.Size = new System.Drawing.Size(35, 37);
+            this.scorePoints.TabIndex = 3;
+            this.scorePoints.Text = "0";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(13, 13);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(143, 37);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "Score: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.BackgroundImage = global::PingPongGame.Properties.Resources.ping_pong_ball;
+            this.ball.Image = global::PingPongGame.Properties.Resources.ping_pong_ball;
+            this.ball.Location = new System.Drawing.Point(244, 125);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(35, 35);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.DimGray;
+            this.player.Location = new System.Drawing.Point(168, 279);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(200, 20);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 365);
+            this.Controls.Add(this.gameBackGround);
+            this.Name = "Form1";
+            this.Text = "PongGame";
+           
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.gameBackGround.ResumeLayout(false);
+            this.gameBackGround.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gameBackGround;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label scorePoints;
+        private System.Windows.Forms.Label gameOver;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPongGame
+{
+    public partial class Form1 : Form
+    {
+
+        public int xSpeed = 7;
+        public int ySpeed = 7;        // Ball speed
+        public int score = 0;           // Score
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None;    // Remove border
+            this.TopMost = true;                            // Form to the front
+            this.Bounds = Screen.PrimaryScreen.Bounds;      // Fullscreen
+
+            player.Top = gameBackGround.Bottom - (gameBackGround.Bottom / 10);  // Set position of player
+
+            gameOver.Left = (gameBackGround.Width / 2) - (gameOver.Width / 2);  // Textbox of gameOver position to center
+            gameOver.Top = (gameBackGround.Height / 2) - (gameOver.Height / 2);
+            gameOver.Visible = false;                                           // Textbox of gameOver hide
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            player.Left = Cursor.Position.X - (player.Width / 2);       // Set the center of the player to the position of the cursor
+
+            ball.Left += xSpeed;         // Move the ball
+            ball.Top += ySpeed;
+
+            if (ball.Bottom >= player.Top &&
+                ball.Bottom >= player.Bottom &&
+                ball.Left >= player.Left &&
+                ball.Right <= player.Right)     // Collision
+            {
+                ySpeed = -ySpeed;           // Change direction
+                score += 1;
+                scorePoints.Text = score.ToString();
+            }
+
+            if (ball.Left <= gameBackGround.Left)
+            {
+                xSpeed = -xSpeed;
+            }
+
+            if (ball.Right >= gameBackGround.Right)
+            {
+                xSpeed = -xSpeed;
+            }
+
+            if (ball.Top <= gameBackGround.Top)
+            {
+                ySpeed = -ySpeed;
+            }
+
+            if (ball.Bottom >= gameBackGround.Bottom)
+            {
+                timer1.Enabled = false;         // Ball is out, stop the game
+                gameOver.Visible = true;        // Show textbox of gameOver
+            }
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();               // Esc to stop the game
+            }
+            
+            if (e.KeyCode == Keys.F1)       // Restart game in EASY mode
+            {
+                ball.Top = 10;
+                ball.Left = 10;
+                xSpeed = 7;
+                ySpeed = 7;
+                score = 0;
+                scorePoints.Text = "0";
+                timer1.Enabled = true;
+                gameOver.Visible = false;
+            }
+
+            if (e.KeyCode == Keys.F2)       // Restart game in HARD mode
+            {
+                ball.Top = 10;
+                ball.Left = 10;
+                xSpeed = 15;
+                ySpeed = 15;
+                score = 0;
+                scorePoints.Text = "0";
+                timer1.Enabled = true;
+                gameOver.Visible = false;
+            }
+        }
+    }
+}
